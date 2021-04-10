7cd3fa9999b1dbb6c9817fc3e11b9fbb91391ef2
Arnpo18 <79510111+Arnpo18@users.noreply.github.com>
2021-04-10 15:36:52 +0200

    lluita casi

---
+++ JOC/joc pong/PONG 2J.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	OS.set_window_size(Vector2(1024, 600)) # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
