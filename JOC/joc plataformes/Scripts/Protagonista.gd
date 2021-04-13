6e583112c7171b24ac962e9dea276efa1195a1d5
Ridotchet <rollmartvict@gmail.com>
2021-04-13 10:41:24 +0200

    Protagonista
    Moviment i animació del protagonista

--- JOC/joc plataformes/Scripts/Protagonista.gd
+++ JOC/joc plataformes/Scripts/Protagonista.gd
@@ -2,7 +2,6 @@ extends KinematicBody2D
 
 var velocitat : Vector2
 var salt_aeri = 1
-var anima_segonsalt : bool
 
 
 func _ready():
@@ -10,26 +9,16 @@ func _ready():
 
 func _process(delta):
 	# Moviment del jugador
-	velocitat.x = 0
 	if Input.is_action_pressed("dreta_plataformes"):
 		velocitat.x = 100
-		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed("esquerra_plataformes"):
 		velocitat.x = -100
-		$AnimatedSprite.flip_h = true
-	if Input.is_action_just_pressed("salta_plataformes"):
-		if is_on_floor():
-			velocitat.y = -200
-			$AnimatedSprite.play('Jump')
-		elif salt_aeri == 1:
-			velocitat.y = -150
-			salt_aeri = 0
-			$AnimatedSprite.play('Double Jump')
+	if Input.is_action_pressed("salta_plataformes"):
+		velocitat.y = -100
 	
 	if is_on_floor() and salt_aeri == 0:
 		salt_aeri = 1
-	
-	velocitat.y += 100*delta
+#	velocitat.y += 100*delta
 	
 	velocitat = move_and_slide(velocitat, Vector2.UP)
 	
@@ -40,5 +29,7 @@ func anima(velocitat):
 		$AnimatedSprite.play('Idle')
 	if velocitat.y > 0:
 		$AnimatedSprite.play('Fall')
-	if velocitat.x != 0 and velocitat.y == 0:
-		$AnimatedSprite.play('Run')
+	if Input.is_action_just_pressed("salta_plataformes") and is_on_floor():
+		$AnimatedSprite.play('Jump')
+	if Input.is_action_just_pressed("salta_plataformes") and not is_on_floor() and salt_aeri == 1:
+		$AnimatedSprite.play('Double Jump')
