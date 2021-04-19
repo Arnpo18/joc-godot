e88e4e71bef77b211818e4aa77221b48a3057b22
Ridotchet <79510345+Ridotchet@users.noreply.github.com>
2021-04-19 18:39:22 +0200

    General Update
    Mapa zona 1
    Protagonista update
    Enemic 1 funcional

--- JOC/Protagonista.gd
+++ JOC/Protagonista.gd
@@ -1,39 +1,45 @@
 extends KinematicBody2D
 
+var animacio_inicial = false
 var velocitat : Vector2
-var salt_aeri = 1
+var salt_aeri = false
+var gravetat = 200
+var moviment = 150
+var salt = 200
+var segon_salt = 150
 
 
 func _ready():
+	$AnimacioInicial.start()
 	$AnimatedSprite.play('Appearing')
-	pass
 
-func _process(delta):
-	# Moviment del jugador
-	velocitat.x = 0
-	if Input.is_action_pressed("dreta_plataformes"):
-		velocitat.x = 100
-		$AnimatedSprite.flip_h = false
-	if Input.is_action_pressed("esquerra_plataformes"):
-		velocitat.x = -100
-		$AnimatedSprite.flip_h = true
-	if Input.is_action_just_pressed("salta_plataformes"):
+func _physics_process(delta):
+	if animacio_inicial == true:
+		# Moviment del jugador
+		velocitat.x = 0
+		if Input.is_action_pressed("dreta_plataformes"):
+			velocitat.x = moviment
+			$AnimatedSprite.flip_h = false
+		if Input.is_action_pressed("esquerra_plataformes"):
+			velocitat.x = -moviment
+			$AnimatedSprite.flip_h = true
+		if Input.is_action_just_pressed("salta_plataformes"):
+			if is_on_floor():
+				velocitat.y = -salt
+				$AnimatedSprite.play('Jump')
+			elif salt_aeri == true:
+				velocitat.y = -segon_salt
+				salt_aeri = false
+				$AnimatedSprite.play('Double Jump')
+		
 		if is_on_floor():
-			velocitat.y = -200
-			$AnimatedSprite.play('Jump')
-		elif salt_aeri == 1:
-			velocitat.y = -150
-			salt_aeri = 0
-			$AnimatedSprite.play('Double Jump')
-	
-	if is_on_floor() and salt_aeri == 0:
-		salt_aeri = 1
-	
-	velocitat.y += 100*delta
-	
-	velocitat = move_and_slide(velocitat, Vector2.UP)
-	
-	anima(velocitat)
+			salt_aeri = true
+		
+		velocitat.y += gravetat*delta
+		
+		velocitat = move_and_slide(velocitat, Vector2.UP)
+		
+		anima(velocitat)
 
 func anima(velocitat):
 	if velocitat == Vector2.ZERO:
@@ -42,3 +48,10 @@ func anima(velocitat):
 		$AnimatedSprite.play('Fall')
 	if velocitat.x != 0 and velocitat.y == 0:
 		$AnimatedSprite.play('Run')
+
+func _on_AnimacioInicial_timeout():
+	animacio_inicial = true
+
+
+func mal(quantitat):
+	pass
--- JOC/joc plataformes/Scripts/AngryPig.gd
+++ JOC/joc plataformes/Scripts/AngryPig.gd
@@ -1,15 +1,81 @@
 extends KinematicBody2D
 
-var velocitat
+var velocitat : Vector2
+var gravetat = 200
+var target : KinematicBody2D
+var target_no : KinematicBody2D
+var mov_dreta = false
+var mov_esq = true
+var quiet_1 = false
+var quiet_2 = false
+var moviment = 100
 
 func _ready():
-	pass
+	$Caminar.start()
 
 
-func _process(delta):
+func _physics_process(delta):
 	
+	if target:
+		$AnimatedSprite.play('Run')
+		if target.position.x > position.x:
+			velocitat.x = moviment*1.5
+		if target.position.x < position.x:
+			velocitat.x = -moviment*1.5
+		if target.position.x - position.x == 100 or position.x - target.position.x == 100:
+			velocitat.x = 0
+		
+	
+	else:
+		if mov_dreta == true:
+			velocitat.x = moviment
+		elif quiet_1 == true:
+			velocitat.x = 0
+		elif mov_esq == true:
+			velocitat.x = -moviment
+		elif quiet_2 == true:
+			velocitat.x = 0
+			
+	if velocitat.x > 0:
+		$AnimatedSprite.flip_h = true
+	if velocitat.x <= 0:
+		$AnimatedSprite.flip_h = false
+	
+	velocitat.y += gravetat*delta
+	velocitat = move_and_slide(velocitat, Vector2.UP)
 	
 	anima(velocitat)
 
 func anima(velocitat):
-	pass
+	if not target:
+		if velocitat.x == 0:
+			$AnimatedSprite.play('Idle')
+		if velocitat.x != 0:
+			$AnimatedSprite.play('Walk')
+
+
+func _on_Caminar_timeout():
+	if mov_dreta == true:
+		mov_dreta = false
+		quiet_1 = true
+	elif quiet_1 == true:
+		quiet_1 = false
+		mov_esq = true
+	elif mov_esq == true:
+		mov_esq = false
+		quiet_2 = true
+	elif quiet_2 == true:
+		quiet_2 = false
+		mov_dreta = true
+	$Caminar.start()
+
+
+
+func _on_Vision_body_entered(body):
+	if body.has_method('mal'):
+		target = body
+
+
+func _on_Vision_body_exited(body):
+	if body.has_method('mal'):
+		target = target_no
